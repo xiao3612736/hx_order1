d49e9fb6077b1237f87c8ed23132cc26b4efcde7
ZhangXiao <a3612736>
2017-10-21 14:59:13 +0800

    add view

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ var expressValidator = require('express-validator');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var pkg = require('./package.json');
+var fs = require('fs');
 
 var app = express();
 app.appName = 'hx_order';
 
 // require('../config/database')(app.config.database.url, mongoose);
-
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
 
 app.set('env', env);
 app.set('port', 3000);
@@ -32,6 +29,16 @@ app.use(bodyParser.text({ type: 'text/*' }));
 app.use(expressValidator());
 app.use(methodOverride());
 app.use(cookieParser(pkg.name));
+
+// var modulePath = __dirname + '/' + 'controller';
+// fs.readdirSync(modulePath).forEach(function (file) {
+//     console.log('file---------------->', file);
+//     require('./controller/'+file);
+// });
+
+app.get('/test', function (req, res) {
+    res.render('index');
+});
 
 var server = app.listen(app.get('port'), function () {
     console.log("✔ Express server [" + app.appName + "] listening on port %d in %s mode", app.get('port'), app.get('env'));
